0e137b922fa1a9a11d30f0b10f3f0fba9b033f21
eunjin0212 <eunjin9639@gmail.com>
2023-05-21 22:11:51 +0900

    👽️ fetch movies list

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,23 @@
+import { useEffect, useState } from 'react'
+
+const API_KEY = '3d85994113faadbad92f020ed39c18f4'
 
 export default function Home() {
+  const [movies, setMovies] = useState()
+  useEffect(() => {
+    (async () => {
+      const { results } = await (await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`)).json()
+      setMovies(results)
+    })()
+  }, [])
   return (
     <div>
-      Home
+      {!movies && <h4>Loading...</h4>}
+      {movies?.map((movie) => (
+        <div key={movie.id}>
+          {movie.original_title}
+        </div>
+      ))}
     </div>
   )
 }
