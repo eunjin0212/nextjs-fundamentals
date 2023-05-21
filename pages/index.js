32985c7838392e3227a76f3396a444724e1942d9
eunjin0212 <eunjin9639@gmail.com>
2023-05-21 17:45:05 +0900

    💄 globals.css, _app.js
    Next.js는 App 컴포넌트를 사용하여 page를 초기화함. 이를 재정의하고 페이지 초기화를 제어할 수 있다.
    
    1. 페이지 변경 간에 레이아웃 유지
    2. 페이지 탐색 시 state 유지
    3. componentDidCatch를 사용한 Custom 에러 처리
    4. 페이지에 추가 데이터 삽입
    5. Global CSS 추가

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
-import NavBar from '@/components/NavBar';
-
 export default function Home () {
   return (
     <div>
-      <NavBar />
       Home
+      <style jsx global>{`
+        a {
+          color: green
+          // 왜 about 페이지는 적용이 안될까? 페이지 별로 따로 따로 생각해야함 스코프가 있음 
+        }
+      `}</style>
     </div>
   )
 }
