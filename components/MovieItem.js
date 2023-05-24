9cd16fe0d644a20e542f549280301b8519ca6275
eunjin0212 <eunjin9639@gmail.com>
2023-05-24 20:59:07 +0900

    ✨ Dynamic Routes

---
+++ components/MovieItem.js
@@ -0,0 +1,27 @@
+export default function MovieItem({ movie }) {
+  return (
+    <div className='movie'>
+      {/* eslint-disable-next-line @next/next/no-img-element */}
+      <img
+        src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+        alt={movie.original_title}
+      />
+      <h4>{movie.original_title}</h4>
+      <style jsx>{`
+        .movie img {
+          max-width: 100%;
+          border-radius: 12px;
+          transition: transform 0.2s ease-in-out;
+          box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
+        }
+        .movie:hover img {
+          transform: scale(1.05) translateY(-10px);
+        }
+        .movie h4 {
+          font-size: 18px;
+          text-align: center;
+        }
+      `}</style>
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,10 @@
+import MovieItem from "@/components/MovieItem";
+
 export default function Home({ results }) {
   return (
-    <div className="container">
+    <div className='container'>
       {results?.map((movie) => (
-        <div className="movie" key={movie.id}>
-          {/* eslint-disable-next-line @next/next/no-img-element */}
-          <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt={movie.original_title} />
-          <h4>{movie.original_title}</h4>
-        </div>
+        <MovieItem movie={movie} key={movie.id} />
       ))}
       <style jsx>{`
         .container {
@@ -15,28 +13,17 @@ export default function Home({ results }) {
           padding: 20px;
           gap: 20px;
         }
-        .movie img {
-          max-width: 100%;
-          border-radius: 12px;
-          transition: transform 0.2s ease-in-out;
-          box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
-        }
-        .movie:hover img {
-          transform: scale(1.05) translateY(-10px);
-        }
-        .movie h4 {
-          font-size: 18px;
-          text-align: center;
-        }
       `}</style>
     </div>
-  )
+  );
 }
 export async function getServerSideProps() {
-  const { results } = await (await fetch('http://localhost:3000/api/movies')).json()
+  const { results } = await (
+    await fetch("http://localhost:3000/api/movies")
+  ).json();
   return {
     props: {
       results,
-    }
-  }
+    },
+  };
 }
